6d5caf5385dc10090e565f2dd96a383b0c306fd3
Dinovi Sanchez <dinovi.sanchez@bia.app>
2026-04-04 01:43:01 -0500

    feat: maxDuration 90s

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: { bodySizeLimit: "10mb" },
+  },
+  maxDuration: 90,
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "drive.google.com", pathname: "/**" },
